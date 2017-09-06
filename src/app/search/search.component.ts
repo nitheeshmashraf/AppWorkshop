d33ea8bdc71b0f0ef859bbca90c2f88ebee86bc6
nitheeshmashraf <nitheeshmashraf@gmail.com>
2017-09-06 18:23:51 +0400

    SearchComplete

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,14 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { Http,Response } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
+
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  private apiURL='https://pixabay.com/api/?key=6361417-ad97c2d131a8c928dd6f4d85d&pretty=true&q=';
+  data:any={};
+  SearchTerm:string ='Lion';
 
+  constructor(private http:Http) {
+    this.getData();
+   }
+
+   getData(){
+     return this.http.get(this.apiURL+this.SearchTerm)
+     .map((res:Response)=>res.json())
+     .subscribe(data=>{
+       this.data=data;
+     })
+   }
+
+  
   ngOnInit() {
   }
 
